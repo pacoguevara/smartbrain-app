ca525e60f6569b49708e7ce0779a88868a254be7
Paco Guevara <pacoguevaraa@gmail.com>
2019-03-18 18:36:50 -0600

    detect faces and display the box

--- src/App.js
+++ src/App.js
@@ -4,16 +4,59 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from 'react-particles-js'
+import Clarifai from 'clarifai'
+const app = new Clarifai.App({
+ apiKey: '5a756dd3f68343958546cd4db3b65ae9'
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, 
+      this.state.input).then(response => this.displayBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
         <Particles className='particles' params={{
                 particles: {
                   line_linked: {
@@ -32,9 +75,7 @@ class App extends Component {
                   }
                 }
               }} />
-        {/*
-                
-                <FaceRecognition />*/}
+        <FaceRecognition imageUrl={ this.state.imageUrl } box={ this.state.box }/>
       </div>
     );
   }
